1fd0c5fe1df7a6aca2d4800c92e56e21c2d58d34
Jintong Wu <akudet@qq.com>
2016-07-19 14:32:12 +0800

    add list excersice

---
+++ java1607/basics/src/list_ex/Main.java
@@ -0,0 +1,81 @@
+package list_ex;
+
+import java.util.*;
+
+public class Main {
+	private static List<User> users = new ArrayList<User>(5);
+	
+	
+
+	public static void main(String[] args) {
+		addUsers();
+
+		User user = login();
+		
+		if (null == user) {
+			System.out.println("Login failed: system shutting down");
+			System.exit(-1);
+		} else {
+			System.out.println("Welcome " + user.getName());
+		}
+
+	}
+
+	private static void addUsers() {
+		users.add(new User("jtwu", "123"));
+		users.add(new User("zaft", "321"));
+		users.add(new User("narukami", "222"));
+		users.add(new User("kugimi", "222"));
+		users.add(new User("tiger", "222"));
+		users.add(new User("ÇÍ±Ú", "222"));
+		users.add(new User("ÑùÊ½", "222"));
+	}
+
+	private static User login() {
+		Scanner scan = new Scanner(System.in);
+		
+		for (int i = 3; i > 0; i--) {
+			System.out.print("Please enter username : ");
+			System.out.flush();
+			
+			String name = null;
+			if (scan.hasNext()) {
+				name = scan.next();
+			} else {
+				System.exit(0);
+			}
+			
+			System.out.print("Please enter passwaord : ");
+			System.out.flush();
+			
+			String password = null;
+			if (scan.hasNext()) {
+				password = scan.next();
+			} else {
+				System.exit(0);
+			}
+			
+			// check whether there got a match for the user
+			User user = new User(name, password);
+			int index = users.indexOf(user);
+			if (-1 == index) {
+				System.out.println("Wrong username or password");
+				System.out.println("Please Reatry, you have " + (i - 1)+ " time(s) remains");
+			} else {
+				return users.get(index);
+			}
+		}
+		
+		return null;
+	}
+
+	private static User findUserByName(String name, List<User> users) {
+		for (User user : users) {
+			if (name.equals(user.getName())) {
+				return user;
+			}
+		}
+		return null;
+	}
+	
+}
---
+++ java1607/basics/src/list_ex/User.java
@@ -0,0 +1,54 @@
+package list_ex;
+
+public class User {
+	
+	private String name;
+	private String password;
+	
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		User other = (User) obj;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		if (password == null) {
+			if (other.password != null)
+				return false;
+		} else if (!password.equals(other.password))
+			return false;
+		return true;
+	}
+
+	public User(String name, String password) {
+		super();
+		this.name = name;
+		this.password = password;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+	
+
+}
