4b57075ae404ff383638fcb7196bd12e6d8b2e64
Jintong Wu <akudet@qq.com>
2016-07-20 10:49:16 +0800

    using TreeMap sort student

--- java1607/basics/src/ex_15/Student.java
+++ java1607/basics/src/ex_15/Student.java
@@ -1,8 +1,34 @@
 package ex_15;
 
-public class Student {
+public class Student implements Comparable<Student> {
+	@Override
+	public int compareTo(Student o) {
+		int res = this.age - o.age;
+		if (res == 0) {
+			return id.compareTo(o.id);
+		}
+		return res;
+	}
+	
+	@Override
+	public String toString() {
+		StringBuilder sb = new StringBuilder();
+		sb.append(name + " : ");
+		sb.append(age);
+		return sb.toString();
+	}
+
 	private String id;
 	private String name;
+	private int age;
+	
+	public Student(String id, String name, int age) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.age = age;
+	}
+	
 	public Student(String id, String name) {
 		super();
 		this.id = id;
--- java1607/basics/src/ex_15/StudentDemo.java
+++ java1607/basics/src/ex_15/StudentDemo.java
@@ -5,18 +5,33 @@ import java.util.*;
 public class StudentDemo {
 	
 	public static void main(String... args) {
-		HashMap<Integer, Student> hm = new HashMap<Integer, Student>();
+		HashMap<Student, Integer> hm = new HashMap<Student, Integer>();
 		
-		hm.put(1, new Student("123", "jtwu"));
-		hm.put(2, new Student("124", "zaft"));
-		hm.put(3, new Student("125", "narukami"));
+		hm.put(new Student("123", "jtwu", 22),  1);
+		hm.put(new Student("125", "narukami", 32), 4);
+		hm.put(new Student("124", "zaft", 22), 4);
 		
-		hm.put(1, new Student("222", "wataru"));
+		hm.put(new Student("222", "wataru", 111), 2);
+		
+		Student s1 = new Student("123", "jtwu", 22);
+		Student s2 = new Student("124", "zaft", 22);
+		
+		TreeSet<Student> ts = new TreeSet<Student>();
+		ts.add(s1);
+		ts.add(s2);
+		System.out.println("set size : " + ts.size());
 		
 		System.out.println(hm.size());
 		
+		for (Student key : hm.keySet()) {
+			System.out.println(key.getName());
+		}
 		
+		TreeMap<Student, Integer> tm = new TreeMap<Student, Integer>(hm);
 		
+		for (Student key : tm.keySet()) {
+			System.out.println(key);
+		}
 	}
 
 }
