4f8279e1a96676cfdd5e214f7fe442d79a6d1917
Jintong Wu <akudet@qq.com>
2016-07-20 14:12:48 +0800

    add inclass excersice stub

---
+++ java1607/basics/src/inclass_15/InClass.java
@@ -0,0 +1,56 @@
+package inclass_15;
+
+import java.util.*;
+
+public class InClass {
+	
+	public static void main(String[] args) {
+		test1();
+		test2();
+		test3();
+		test4();
+		test5();
+	}
+
+	/**
+	 * 编写学生类，创建一个properties文件，将学生类的信息相对应的存储在文件中。
+	 * 读取文件信息，赋给学生对象，并将学生对象存储在集合中。
+	 */
+	private static void test5() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	/**
+	 * 用TreeMap存储学生对象信息，按照学生年龄进行排序。
+	 */
+	private static void test4() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	/**
+	 * 编写学生类， 测试类中将多个学生存放在HashMap集合中，key值用int来存储
+	 * 相同int值来存储不同学生。查看集合中的对象。验证HashMap的存储。 
+	 */
+	private static void test3() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	/**
+	 * 用TreeSet存储人对象，按照人的年龄将对象排序存储。
+	 */
+	private static void test2() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	/**
+	 * HashSet存储对象。编写人类。
+	 * 实例化相同信息的人对象，存入到集合中，相同信息的对象不能重复存入。
+	 */
+	private static void test1() {
+	}
+
+}
---
+++ java1607/basics/src/inclass_15/Person.java
@@ -0,0 +1,58 @@
+package inclass_15;
+
+public class Person implements Comparable<Person>{
+	String name;
+	int age;
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + age;
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		return result;
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Person other = (Person) obj;
+		if (age != other.age)
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		return true;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public Person(String name, int age) {
+		super();
+		this.name = name;
+		this.age = age;
+	}
+	@Override
+	public int compareTo(Person o) {
+		int res = new Integer(this.age).compareTo(o.age);
+		if (0 == res) {
+			res = this.name.compareTo(o.name);
+		}
+		return res;
+	}
+
+}
