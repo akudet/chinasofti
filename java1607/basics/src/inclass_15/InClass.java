f416ac6b42c9ada6c038a2756d4869ef5c483cd4
Jintong Wu <akudet@qq.com>
2016-07-20 14:58:19 +0800

    implement test3-5

--- java1607/basics/src/inclass_15/InClass.java
+++ java1607/basics/src/inclass_15/InClass.java
@@ -1,6 +1,8 @@
 package inclass_15;
 
+import java.io.*;
 import java.util.*;
+import java.util.Map.Entry;
 
 public class InClass {
 	
@@ -17,16 +19,106 @@ public class InClass {
 	 * 读取文件信息，赋给学生对象，并将学生对象存储在集合中。
 	 */
 	private static void test5() {
-		// TODO Auto-generated method stub
+		final String PRO_PATH = "src/inclass_15/";
 		
+		File students_f = new File(PRO_PATH + "students.properties");
+		
+		List<Student> students = new ArrayList<Student>(10);
+		
+		Student s1 = new Student(1, "jtwu", 22);
+		Student s2 = new Student(2, "kyo", 7);
+		Student s3 = new Student(3, "izumi", 1);
+		Student s4 = new Student(4, "nakato", 16);
+		students.add(s1);
+		students.add(s2);
+		students.add(s3);
+		students.add(s4);
+		
+		saveStudentsToFile(students_f, students);
+		
+		// read student from file, and output it
+		System.out.println("Properties:");
+		Properties s_p = new Properties();
+		Set<Student> ss = new HashSet<Student>();
+		try {
+			s_p.load(new FileInputStream(students_f));
+			
+			for (Object key : s_p.keySet()) {
+				Student s = parse((String) s_p.get(key));
+				ss.add(s);
+				System.out.println(s);
+			}
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	private static void saveStudentsToFile(File students_f, Collection<Student> students) {
+		Properties students_p = new Properties();
+		
+		final String comments = "commit by jtwu";
+		
+		try {
+			students_p.load(new FileInputStream(students_f));
+			
+			for (Student s : students) {
+				students_p.setProperty("" + s.getId(), format(s));
+			}
+			
+			students_p.store(new FileOutputStream(students_f), comments);
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+		
+	}
+
+	private static String format(Student s) {
+		StringBuilder sb = new StringBuilder();
+		sb.append(s.getId());
+		sb.append(' ');
+		sb.append(s.getName());
+		sb.append(' ');
+		sb.append(s.getAge());
+		return sb.toString();
+	}
+	
+	private static Student parse(String s) {
+		Scanner scan = new Scanner(s);
+		int id = scan.nextInt();
+		String name = scan.next();
+		int age = scan.nextInt();
+		return new Student(id, name, age);
 	}
 
 	/**
 	 * 用TreeMap存储学生对象信息，按照学生年龄进行排序。
 	 */
 	private static void test4() {
-		// TODO Auto-generated method stub
+		TreeMap<Student, String> students = new TreeMap<Student, String>();
 		
+		Student s1 = new Student(1, "jtwu", 22);
+		Student s2 = new Student(2, "kyo", 7);
+		Student s3 = new Student(3, "izumi", 1);
+		Student s4 = new Student(4, "nakato", 16);
+		
+		students.put(s1, s1.getName());
+		students.put(s2, s2.getName());
+		students.put(s3, s3.getName());
+		students.put(s4, s4.getName());
+		
+		System.out.println("TreeMap:");
+		for (Entry<Student, String> entry : students.entrySet()) {
+			System.out.println(entry.getKey());
+		}
 	}
 
 	/**
@@ -34,8 +126,22 @@ public class InClass {
 	 * 相同int值来存储不同学生。查看集合中的对象。验证HashMap的存储。 
 	 */
 	private static void test3() {
-		// TODO Auto-generated method stub
+		HashMap<Integer, Student> students = new HashMap<Integer, Student>();
 		
+		Student s1 = new Student(1, "jtwu", 22);
+		Student s2 = new Student(2, "kyo", 17);
+		Student s3 = new Student(3, "izumi", 17);
+		Student s4 = new Student(4, "nakato", 16);
+		
+		students.put(s1.getId(), s1);
+		students.put(s2.getId(), s2);
+		students.put(s3.getId(), s3);
+		students.put(s4.getId(), s4);
+		
+		System.out.println("HashMap:");
+		for (Entry<Integer, Student> entry : students.entrySet()) {
+			System.out.println(entry.getValue());
+		}
 	}
 
 	/**
--- java1607/basics/src/inclass_15/Student.java
+++ java1607/basics/src/inclass_15/Student.java
@@ -1,27 +1,16 @@
 package inclass_15;
 
 public class Student implements Comparable<Student> {
-	@Override
-	public int compareTo(Student o) {
-		int res = this.age - o.age;
-		if (res == 0) {
-			return this.name.compareTo(o.name);
-		}
-		return res;
-	}
-	
-	@Override
-	public String toString() {
-		StringBuilder sb = new StringBuilder();
-		sb.append(name + " : ");
-		sb.append(age);
-		return sb.toString();
-	}
-
 	private int id;
-	private String name;
-	private int age;
 	
+	private String name;
+
+	private int age;
+	public Student(int id, String name) {
+		super();
+		this.id = id;
+		this.name = name;
+	}
 	public Student(int id, String name, int age) {
 		super();
 		this.id = id;
@@ -29,22 +18,36 @@ public class Student implements Comparable<Student> {
 		this.age = age;
 	}
 	
-	public Student(int id, String name) {
-		super();
-		this.id = id;
-		this.name = name;
+	@Override
+	public int compareTo(Student o) {
+		int res = new Integer(this.age).compareTo(o.age);
+		if (0 == res) {
+			res = this.name.compareTo(o.name);
+		}
+		return res;
+	}
+	
+	public int getAge() {
+		return age;
 	}
 	public int getId() {
 		return id;
 	}
-	public void setId(int id) {
-		this.id = id;
-	}
 	public String getName() {
 		return name;
 	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
 	public void setName(String name) {
 		this.name = name;
+	}
+	@Override
+	public String toString() {
+		return "Student [id=" + id + ", name=" + name + ", age=" + age + "]";
 	}
 
 }
