d04dde68106de1ccb39a89881d74c982aabe6557
Jintong Wu <akudet@qq.com>
2016-07-22 10:28:47 +0800

    IO : add buffered reader excersice

--- java1607/basics/src/ex15/BufferedIO.java
+++ java1607/basics/src/ex15/BufferedIO.java
@@ -7,16 +7,27 @@ public class BufferedIO {
 	public static void main(String[] args) {
 		FileInputStream fis = null;
 		BufferedInputStream bis = null;
+
+		FileReader fr = null;
+		BufferedReader br = null;
 		
 		try {
 			fis = new FileInputStream("D:\\x.txt");
 			bis = new BufferedInputStream(fis);
 			
+			fr = new FileReader("D:\\y.txt");
+			br = new BufferedReader(fr);
+			
+			String str;
+			while (null != (str = br.readLine())) {
+				System.out.format("RA: %s%n", str);
+			}
+			
 			byte[] buff = new byte[1024];
 			int len;
 			
 			while (-1 != (len = bis.read(buff))) {
-				System.out.println(new String(buff, 0, len));
+				System.out.print(new String(buff, 0, len));
 			}
 			
 
