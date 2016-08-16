8a2e79bb2449c6981f1b893e6665acb36d731c9d
Jintong Wu <akudet@qq.com>
2016-08-16 19:57:09 +0800

    jQuery : add menu req

---
+++ java1607/web/jQuery/menu.js
@@ -0,0 +1,10 @@
+// wicked req : a dropdown menu
+// click on a link should show the menu with slide animation
+// mouse hover the menu-item should change backgruond
+
+// left menu : 
+// click one item, show menu next to the item
+
+
+// maybe a single dropdown menu is enough, by place it in diff
+// position in the document
