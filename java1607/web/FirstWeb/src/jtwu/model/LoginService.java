f8c9ae02680bdf02a1928dc48ca34ca37953bb0e
Jintong Wu <akudet@qq.com>
2016-08-30 14:37:21 +0800

    Servlet : using map to implement err_msg lookup

--- java1607/web/FirstWeb/src/jtwu/model/LoginService.java
+++ java1607/web/FirstWeb/src/jtwu/model/LoginService.java
@@ -1,11 +1,12 @@
 package jtwu.model;
 
+import java.util.HashMap;
 import java.util.Map;
 
 public class LoginService {
 	
 	/*
-	 * if username and user pass match return 0
+	 * if username and user pass match return AUTH_SUCC
 	 * otherwise return err code
 	 * you can get the err msg by getErrMsg(errCode)
 	 */
@@ -37,20 +38,23 @@ public class LoginService {
 	}
 	
 	// not my design required
-	//返回0，登录成功
-	//返回1，登录失败，等待审核
+	//返回1，登录成功
+	//返回0，登录失败，等待审核
 	//返回2，登录失败，审核被拒绝
 	//返回3：登录失败，用户名或密码出错
-	public static int AUTH_SUCC = 0;
-	public static int ERR_PENDING_AUTH = 1;
+	public static int AUTH_SUCC = 1;
+	public static int ERR_PENDING_AUTH = 0;
 	public static int ERR_AUTH_REJECT = 2;
 	public static int ERR_NAME_OR_PASS = 3;
-	private static String[] ERR_MSG = {
-		"",
-		"登录失败，等待审核",
-		"登录失败，审核被拒绝",
-		"登录失败，用户名或密码出错"
-	};
+	private static Map<Integer, String> ERR_MSG;
+
+	static {
+		ERR_MSG = new HashMap<Integer, String>();
+		ERR_MSG.put(AUTH_SUCC, "登录成功");
+		ERR_MSG.put(ERR_PENDING_AUTH, "登录失败，等待审核");
+		ERR_MSG.put(ERR_AUTH_REJECT, "登录失败，审核被拒绝");
+		ERR_MSG.put(ERR_NAME_OR_PASS, "登录失败，用户名或密码出错");
+	}
 
 	/**
 	 * return the error msg of the errCode 
@@ -58,6 +62,6 @@ public class LoginService {
 	 * @return
 	 */
 	public static String getErrMsg(int errCode) {
-		return ERR_MSG[errCode];
+		return ERR_MSG.get(errCode);
 	}
 }
