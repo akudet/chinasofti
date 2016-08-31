37526e7f9f9f6e7b2ca581def1aa51a5054783ce
Jintong Wu <akudet@qq.com>
2016-08-31 12:55:31 +0800

    JDBC : add more about DAO ideas

--- java1607/web/FirstWeb/src/jtwu/model/dao/UserDao.java
+++ java1607/web/FirstWeb/src/jtwu/model/dao/UserDao.java
@@ -11,6 +11,14 @@ package jtwu.model.dao;
 // User class, this is the I state at previous, which is the user
 // use the DAO to get a projection of actual representation, and use
 // User class to wrap the result, em. use DAO to get a User object.
+// @ref http://www.corej2eepatterns.com/DataAccessObject.htm
+// ok I know the 1st one is not right, the second is a litte wrong
+// i think the User is depends on the DAO, which is not, not that much wrong
+// this first one just ignore the meaning of dao completely, just try to
+// merge this two things together, maybe it's not that bad,
+// in User we wrap the operation of data access, we use dao to get this
+// this is the projection and representation problem, which I don't know 
+// which is better.
 public class UserDao {
 
 }
