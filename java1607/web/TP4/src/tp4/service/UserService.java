dff01615cede93f6befaf9547b248f9d63b9421b
Jintong Wu <akudet@qq.com>
2016-09-11 03:08:23 +0800

    TP 4 : add User and Vip specification

---
+++ java1607/web/TP4/src/tp4/service/UserService.java
@@ -0,0 +1,31 @@
+package tp4.service;
+
+import java.util.*;
+
+import tp4.model.vo.User;
+
+public class UserService {
+	
+	// 参考 2.11 操作员管理
+
+	public int add(String username, String userpass, int privilege) {
+		return -1;
+	}
+
+	public int deleteById(String userId) {
+		return -1;
+	}
+
+	public List<User> findAll() {
+		return null;
+	}
+
+	public User findByUsername() {
+		return null;
+	}
+
+	public int updateById(String userId, String userName, String userPass,
+			int privilege) {
+		return -1;
+	}
+}
---
+++ java1607/web/TP4/src/tp4/service/VipService.java
@@ -0,0 +1,35 @@
+package tp4.service;
+
+import java.util.*;
+
+import tp4.model.vo.Vip;
+
+public class VipService {
+
+	//参考 2.7 会员管理
+	
+	public int add(int vipNumber, String name, String sex, String certNumber,
+			String phone, String address, String comment) {
+		return -1;
+
+	}
+
+	public int deleteById(int vipNumber) {
+		return -1;
+	}
+
+	public Vip findById(String vipNumber) {
+		return null;
+	}
+
+	public List<Vip> findByName(String name) {
+		return null;
+	}
+
+	// 根据提供的信息对信息进行修改
+	public int updateById(int vipNumber, String name, String sex,
+			String certNumber, String phone, String address, String comment) {
+		return -1;
+
+	}
+}
