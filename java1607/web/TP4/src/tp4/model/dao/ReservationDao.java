6a0a9469dedd70b611c98c2d6644f64386d89f2b
Jintong Wu <akudet@qq.com>
2016-09-12 14:22:55 +0800

    TP 4 : add work by Zhongxing Fan

---
+++ java1607/web/TP4/src/tp4/model/dao/ReservationDao.java
@@ -0,0 +1,192 @@
+package tp4.model.dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.UUID;
+
+import tp4.model.db.DBConnection;
+import tp4.model.vo.Reservation;
+import tp4.model.vo.User;
+
+/**
+ * 
+ * @author 范中兴
+ *
+ */
+public class ReservationDao {
+	Connection con = null;
+	PreparedStatement pre = null;
+	ResultSet res = null;
+
+	// 添加数据
+	public int add(Reservation reservation) {
+		con = DBConnection.getConnection();
+		String sql = "insert into reservation values(?,?,?,?)";
+
+		try {
+			pre = con.prepareStatement(sql);
+			String udi = UUID.randomUUID().toString();
+            
+			pre.setString(1, reservation.getReservationId());
+			pre.setString(2, reservation.getName());
+			pre.setString(3, reservation.getPhone());
+			pre.setString(4, reservation.getArriveTime());
+			pre.setString(5, reservation.getReserveTime());
+			pre.setString(6, reservation.getReservationTime());
+			pre.setString(7, reservation.getComment());
+			
+			int i = pre.executeUpdate();
+
+			if (i > 0) {
+				return i;
+			}
+
+		} catch (SQLException e) {
+
+			e.printStackTrace();
+		} finally {
+			DBConnection.close(con, pre);
+		}
+		return 0;
+	}
+	
+	
+	public int addAll(Collection<Reservation> reservations) {
+		for (Reservation reservation : reservations) {
+			if (0 != add(reservation)) {
+				return -1;
+			}
+		}
+		return 0;
+	}
+
+	public int deleteAll() {
+		con = DBConnection.getConnection();
+		String sql = "delete from reservation";
+
+		try {
+			pre = con.prepareStatement(sql);
+			int i = pre.executeUpdate();
+			if (i > 0) {
+				return i;
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			DBConnection.close(con, pre);
+		}
+
+		return 0;
+	}
+
+	public int deleteById(String reservationId) {
+		con = DBConnection.getConnection();
+		String sql = "delete from reservation where reservationId = ?";
+
+		try {
+			pre = con.prepareStatement(sql);
+			pre.setString(1, reservationId);
+			int i = pre.executeUpdate();
+			if (i > 0) {
+				return i;
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			DBConnection.close(con, pre);
+		}
+
+		return 0;
+	}
+
+	// 查询数据
+	public ArrayList<Reservation> findAll() {
+		con = DBConnection.getConnection();
+		String sql = "select * from reservation";
+		try {
+			pre = con.prepareStatement(sql);
+			res = pre.executeQuery();
+			ArrayList<Reservation> list = new ArrayList<Reservation>();
+			while (res.next()) {
+				Reservation reservation = new Reservation(res.getString("reservationId"),
+						res.getString("name"), 
+						res.getString("phone"),
+						res.getString("arriveTime"),
+						res.getString("reserveTime"),
+						res.getString("reservationTime"),
+						res.getString("comment"));
+				        
+
+				list.add(reservation);
+			}
+			return list;
+		} catch (SQLException e) {
+
+			e.printStackTrace();
+		} finally {
+			DBConnection.close(con, pre, res);
+		}
+
+		return null;
+
+	}
+
+	// 删除数据
+	// 单查询
+	public Reservation findById(String reservationId) {
+		con = DBConnection.getConnection();
+		String sql = "select * from reservation where reservationId=?";
+		try {
+			pre = con.prepareStatement(sql);
+			pre.setString(1, reservationId);
+			res = pre.executeQuery();
+			if (res.next()) {
+				Reservation reservation = new Reservation(res.getString("reservationId"),
+						res.getString("name"), res.getString("phone"),
+						res.getString("arriveTime"),res.getString("reserveTime"),res.getString("reservationTime"),
+						res.getString("comment"));
+				return reservation;
+			}
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			DBConnection.close(con, pre, res);
+		}
+		return null;
+	}
+
+	// 修改
+	public int update(Reservation reservation) {
+		con = DBConnection.getConnection();
+		String sql = "update reservation set name = ?,phone = ?,arriveTime= ?,reserveTime= ?,reservationTime= ?,comment= ?  where userid= ?";
+		try {
+			pre = con.prepareStatement(sql);
+			pre.setString(1, reservation.getReservationId());
+			pre.setString(2, reservation.getName());
+			pre.setString(3, reservation.getPhone());
+			pre.setString(4, reservation.getArriveTime());
+			pre.setString(4, reservation.getReserveTime());
+			pre.setString(4, reservation.getReservationTime());
+			pre.setString(4, reservation.getComment());
+			int i = pre.executeUpdate();
+			if (i > 0) {
+				return i;
+			}
+
+		} catch (SQLException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally {
+			DBConnection.close(con, pre, res);
+		}
+		return 0;
+	}
+		
+}
--- java1607/web/TP4/src/tp4/model/vo/Reservation.java
+++ java1607/web/TP4/src/tp4/model/vo/Reservation.java
@@ -1,5 +1,105 @@
 package tp4.model.vo;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.UUID;
+
+import tp4.model.db.DBConnection;
+
+/**
+ * 
+ * @author 范中兴
+ *
+ */
 public class Reservation {
+	protected String reservationId;
+	protected String name;
+	protected String phone;
+	protected String arriveTime;
+	protected String reserveTime;
+	protected String reservationTime;
+	protected String comment;
+	public Reservation() {
+
+	}
+
+	public Reservation(String reservationId, String name, String phone, String arriveTime,String reserveTime,String reservationTime,String comment) {
+		super();
+		this.reservationId = reservationId;
+		this.name = name;
+		this.phone = phone;
+		this.arriveTime = arriveTime;
+		this.reserveTime=reserveTime;
+		this.reservationTime=reservationTime;
+		this.comment=comment;
+	}
+
+	
+
+	public String getReservationId() {
+		return reservationId;
+	}
+
+	public void setReservationId(String reservationId) {
+		this.reservationId = reservationId;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public String getArriveTime() {
+		return arriveTime;
+	}
+
+	public void setArriveTime(String arriveTime) {
+		this.arriveTime = arriveTime;
+	}
+
+	public String getReserveTime() {
+		return reserveTime;
+	}
+
+	public void setReserveTime(String reserveTime) {
+		this.reserveTime = reserveTime;
+	}
+
+	public String getReservationTime() {
+		return reservationTime;
+	}
+
+	public void setReservationTime(String reservationTime) {
+		this.reservationTime = reservationTime;
+	}
+
+	public String getComment() {
+		return comment;
+	}
+
+	public void setComment(String comment) {
+		this.comment = comment;
+	}
+
+	@Override
+	public String toString() {
+		return "UserDao [reservationId=" + reservationId + ", name=" + name
+				+ ", phone=" + phone + ", arriveTime=" + arriveTime + ",reserveTime="+reserveTime+",reservationTime="+reservationTime+",comment="+comment+"]";
+	}
 
 }
